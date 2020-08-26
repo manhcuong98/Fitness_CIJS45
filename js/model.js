93a6baf3a4db4f472bfdbbca27b84953144c8f75
mt766157@gmail.com <mt766157>
2020-08-27 02:27:45 +0700

    aaaaaaaaa

--- js/model.js
+++ js/model.js
@@ -1,6 +1,7 @@
 const model = {}
 model.programs = [];
 model.selectedProgram= undefined;
+model.getComment = undefined
 // firebase.firestore().collection("programs").get().then(function(querySnapshot) {
 //     querySnapshot.forEach(function(doc) {
 //         // doc.data() is never undefined for query doc snapshots
@@ -25,4 +26,24 @@ model.loadprograms = async () => {
     model.programs = await getDataFromDocs(respone.docs)
     view.loadPrograms(model.programs)
 }
-
+model.addComment = (id,Comment,User) => {
+    const dataUpdate = {
+        comments: firebase.firestore.FieldValue.arrayUnion({
+            comment : Comment,
+            user : User
+        })
+    }
+    if(Comment!='' || Comment.trim()!='') {
+        firebase.firestore().collection('programs').doc(id).update(dataUpdate)
+    }
+}
+model.loadComments = async (id) => {
+    const respone = await firebase.firestore().collection("programs").doc(id).get()
+    console.log(respone)
+    model.getComment = await getDataFromDoc(respone)
+    console.log(model.getComment.comments)
+    if (model.getComment.comments.length > 0) {
+        //model.comments = model.conversations[0]
+        view.loadCurrentComments(model.getComment.comments)
+    }
+}
