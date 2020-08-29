51f35523fcb00b996a93e144d78070bdb305e961
Manfred11 <manhcuong.qn1998@gmail.com>
2020-08-29 08:47:10 +0700

    update components (login-register)

--- js/components.js
+++ js/components.js
@@ -93,5 +93,115 @@ components.showOneForumPost=`
     `
 
 
+components.loginScreen =`
+<section class="sign-in">
+<div class="container1">
+    <div class="signin-content">
+        <div class="signin-image">
+            <figure>
+                <img src="../image/image-style/81a29540400159.577dcf3623a88.gif"  alt="">
+            </figure>
+            <a href="#" class="signup-image-link" class="redirect-to-register">Create an account</a>
+        </div>
+        
+        <div class="signin-form">
+                <h2 class="form-title">Sign in</h2>
+                <form method="POST" class="register-form" id="login-form">
+                    <div class="form-group">
+                        <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                        <input type="text" name="email" id="name" placeholder="Email" />
+                        
+                    </div>
+                    <div class="form-group">
+                        <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                        <input type="password" name="password" id="your_pass" placeholder="Password" />
+                    </div>
+                    <div class="form-group">
+                        <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                        <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember
+                            me</label>
+                    </div>
+                    <div class="form-group form-button">
+                        <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
+                    </div>
+                </form>
+                <div class="social-login">
+                    <span class="social-label">Or login with</span>
+                    <ul class="socials">
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                        <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+</div>
+</section>
+`
+components.registerScreen = `
+<section class="signup">
+<div class="container1">
+    <div class="signup-content">
+        <div class="signup-form">
+            <div>
+                <h2 class="form-title">Sign up</h2>
+            </div>
+            <form method="POST" class="register-form" id="register-form">
+                <div class="form-group">
+                    <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                    <input type="text" name="yourName" id="name" placeholder="Your Name" />
+                    <div class="error" id="your-name-error"></div>
+                </div>
+                <div class="form-group">
+                    <label for="email"><i class="zmdi zmdi-email"></i></label>
+                    <input type="email" name="email" id="email" placeholder="Your Email" />
+                    <div class="error" id="email-error"></div>
+                </div>
+                <div class="form-group">
+                    <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                    <input type="password" name="password" id="pass" placeholder="Password" />
+                </div>
+                <div class="form-group">
+                    <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                    <input type="password" name="confirmPassword" id="re_pass" placeholder="Repeat your password" />
+                </div>
+                <div class="form-group styleGender">
+                    <div class="flex1">
+                        <label for="re-pass"><i class="zmdi zmdi-male-female"></i></label>
+                        <select name="Sex" id="dropdownid" aria-placeholder="sex" required>
+                            <option value="" disabled="disabled" selected="selected">Gender</option>
+                            <i class="zmdi zmdi-chevron-down"></i>
+                            <option value="0">Male</option>
+                            <option value="1">Female</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group styleForm">
+                    <div style="margin-right: 5px;">
+                        <input type="text" placeholder="Height">
+                    </div>
+                    <div style="margin-left: 5px;">
+                        <input type="text" placeholder="Weight">
+                    </div>
+                </div>
 
-    
+                <div class="form-group">
+                    <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                    <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all
+                        statements in <a href="#" class="term-service">Terms of service</a></label>
+                </div>
+                <div class="form-group form-button">
+                    <input type="submit" name="signup" id="signup" class="form-submit" value="Register" />
+                </div>
+
+            </form>
+        </div>
+        <div class="signup-image">
+            <figure><img src="../image/image-style/81a29540400159.577dcf3623a88.gif" alt="sing up image">
+            </figure>
+            <a href="#" class="signup-image-link" id="redirect-to-login">I am already member</a>
+        </div>
+    </div>
+</div>
+</section>
+`
--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,71 @@
 let controller = {}
+
+controller.register = (data) => {
+    if(data.firstName.trim() === ''){
+        document.getElementById('first-name-error').innerText = 'Plese input First name' ;
+    } 
+    else{
+        document.getElementById('first-name-error').innerText ='' ; 
+    };
+    if(data.lastName.trim() === ''){
+        document.getElementById('last-name-error').innerText = 'Plese input Last name' ;
+    } 
+    else{
+        document.getElementById('last-name-error').innerText ='' ; 
+    } ;
+    if(data.email.trim() === ''){
+        document.getElementById('email-error').innerText = 'Plese input Email' ;
+    } 
+    else{
+        document.getElementById('email-error').innerText ='' ; 
+    } ;
+
+    if(data.password.trim() ===''){
+        document.getElementById('password-error').innerText = 'Plese input Password' ;
+    } 
+    else{
+        document.getElementById('password-error').innerText ='' ; 
+    }
+    if(data.confirmPassword.trim() === ''){
+        document.getElementById('confirm-password-error').innerText = 'Plese input Confirm password' ;
+    } 
+    else if(data.password !== data.confirmPassword){
+        document.getElementById('confirm-password-error').innerText ="Password didn't match"
+    }
+    else {
+        document.getElementById('confirm-password-error').innerText ='' ; 
+    } ;
+    if(data.firstName !== '' && data.lastName !== ''&& data.email !== '' && data.password !== '' &&
+       data.confirmPassword !=='' && data.password ===data.confirmPassword){
+           model.register(data);
+       }
+    
+   
+};
+
+controller.login = (dataLogin) => {
+    if(dataLogin.email === ''){
+        document.getElementById('email-error').innerText = 'Plese input Email' ;
+    } 
+    else{
+        document.getElementById('email-error').innerText ='' ; 
+    } ;
+
+    if(dataLogin.password ===''){
+        document.getElementById('password-error').innerText = 'Plese input Password' ;
+    } 
+    else{
+        document.getElementById('password-error').innerText ='' ; 
+    }
+    if(dataLogin.email !== '' &&
+    dataLogin.password !== '')
+    {
+        model.login(dataLogin);
+    }
+};
+
+
+
 controller.changeGender = (num) => {
     if (num == 0) return "Male";
     if (num == 1) return "Female"
