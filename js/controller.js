fb2f5f64868f114b021c516b0ca46f54ded75745
mt766157@gmail.com <mt766157>
2020-09-06 21:13:25 +0700

    kk

--- js/controller.js
+++ js/controller.js
@@ -46,6 +46,6 @@ controller.register = (data) => {
 }
 controller.changeGender = (num) => {
     if (num == 0) return "Male";
-    if (num == 1) return "Female"
-    if (num == 2) return "Both"
+    if (num == 1) return "Female";
+    if (num == 2) return "Both";
 }
